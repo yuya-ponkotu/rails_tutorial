4df52411061fa94d7e12b5e2d98fa7423ccda612
yuya-ponkotu <yuya@yuya.com>
2022-02-24 17:34:06 +0900

    Add user microposts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,14 @@
 class ApplicationController < ActionController::Base
-    def hello
-      render html: "hello, world!"
+  include SessionsHelper
+
+  private
+
+    # ユーザーのログインを確認する
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
     end
-    include SessionsHelper
 end
